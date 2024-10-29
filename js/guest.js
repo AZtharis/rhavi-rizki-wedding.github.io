16ced43cdfc44491afbd5ae4dbe009736a23446e
Budi Pekerti <rhavihakiki@gmail.com>
2024-10-29 17:55:25 +0700

    Sampai tanggal pernikahan

--- js/guest.js
+++ js/guest.js
@@ -11,14 +11,14 @@ export const guest = (() => {
         const until = document.getElementById('count-down').getAttribute('data-time').replace(' ', 'T');
         const count = (new Date(until)).getTime();
 
-        setInterval(() => {
+        /*setInterval(() => {
             const distance = Math.abs(count - (new Date()).getTime());
 
             document.getElementById('day').innerText = Math.floor(distance / (1000 * 60 * 60 * 24));
             document.getElementById('hour').innerText = Math.floor((distance % (1000 * 60 * 60 * 24)) / (1000 * 60 * 60));
             document.getElementById('minute').innerText = Math.floor((distance % (1000 * 60 * 60)) / (1000 * 60));
             document.getElementById('second').innerText = Math.floor((distance % (1000 * 60)) / 1000);
-        }, 1000);
+        }, 1000);*/
     };
 
     const animation = () => {
